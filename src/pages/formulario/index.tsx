512e0379095f0e687ca9c102c699b1d367d9b3ec
Jollyn Saint Hilaire <67757929+Jollynjose@users.noreply.github.com>
2024-10-31 08:05:37 -0400

    feat: Update layout and styling for Formulario page

--- src/pages/formulario/index.tsx
+++ src/pages/formulario/index.tsx
@@ -20,11 +20,18 @@ function Home() {
             flexDirection: 'row',
             alignItems: 'center',
             justifyContent: 'space-between',
+            gap: { xs: '1rem', md: '0' },
           }}
         >
           <Typography variant="h4">Formulario</Typography>
 
-          <Button variant="contained" color="primary">
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{
+              width: 'min-content',
+            }}
+          >
             <Typography variant="body1">Administrar Datos</Typography>
           </Button>
         </Box>
@@ -37,7 +44,7 @@ function Home() {
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
-            padding: '0rem 1rem 1rem 1rem',
+            padding: { xs: '0rem 0rem 1rem 0rem', md: '0rem 1rem 1rem 1rem' },
           }}
         >
           <ViaticosForm />
